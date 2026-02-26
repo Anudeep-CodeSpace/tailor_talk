6ced04b5b4095de7c4f608680b585b89f1003922
Anudeep <anudeep@Anudeeps-MacBook-Air.local>
2026-02-26 19:43:47 +0530

    Port Issue

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 def main():
     print('Starting the backend Server....\n')
-    uvicorn.run(app=app, host="0.0.0.0", port=os.getenv('PORT'))
+    uvicorn.run(app=app, host="0.0.0.0", port=int(os.getenv('PORT')))
 
 
 if __name__ == "__main__":
