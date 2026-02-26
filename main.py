559a3fdcfcc137dde324858ac081dd3ec9d0d92d
Anudeep <anudeep@Anudeeps-MacBook-Air.local>
2026-02-26 19:30:22 +0530

    Port Value changable

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import uvicorn
 from backend.app import app
+import os
 
 def main():
     print('Starting the backend Server....\n')
-    uvicorn.run(app=app, host="0.0.0.0", port=8000)
+    uvicorn.run(app=app, host="0.0.0.0", port=os.getenv('PORT'))
 
 
 if __name__ == "__main__":
